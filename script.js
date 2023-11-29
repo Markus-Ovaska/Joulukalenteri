d01cc48a562ca3afea09684ee1e13f2340aa3b11
bhi060@myy.haaga-helia.fi <bhi060@myy.haaga-helia.fi>
2023-11-29 15:48:53 +0200

    valmis?

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function openPopup(day, content) {
     // Check if the clicked day is the current day
     var currentDate = new Date();
     var currentDay = currentDate.getDate();
-    
+
 
     if (day <= currentDay) {
         // Get the popup element
         var popup = document.getElementById('popup');
-        
+
         // Set the content of the popup
         popup.innerHTML = content;
 
@@ -19,25 +19,3 @@ function openPopup(day, content) {
         alert("Ei vielä tänne hätähousu.");
     }
 }
-
-//let today = new Date();
-//let today1 = today.getDate();
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    var today = new Date();
-    var dayOfWeek = today.getDay();
-    var table = document.getElementById("table"); 
-
-    // Assuming your table has a consistent structure, you may need to adjust this loop
-    for (var i = 0; i < table.rows.length; i++) {
-      var row = table.rows[i];
-      var cell = row.cells[dayOfWeek];
-
-      if (cell) {
-        cell.classList.add("current-day");
-      }
-    }
-  }
-  );
