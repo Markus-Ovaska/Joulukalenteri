3db11c299449d27fcff480a08d573be18acb3efe
bhi060@myy.haaga-helia.fi <bhi060@myy.haaga-helia.fi>
2023-11-29 14:51:24 +0200

    gg Co-authored-by: Markus-Ovaska <Markus-Ovaska@users.noreply.github.com>

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ function openPopup(day, content) {
         alert("Ei vielä tänne hätähousu.");
     }
 }
+
+//let today = new Date();
+//let today1 = today.getDate();
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var today = new Date();
+    var dayOfWeek = today.getDay();
+    var table = document.getElementById("table"); 
+
+    // Assuming your table has a consistent structure, you may need to adjust this loop
+    for (var i = 0; i < table.rows.length; i++) {
+      var row = table.rows[i];
+      var cell = row.cells[dayOfWeek];
+
+      if (cell) {
+        cell.classList.add("current-day");
+      }
+    }
+  }
+  );
